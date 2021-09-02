20ed17e85e45f102ba200b0e09c5563952d7e5db
xersuy <dev.luyoes20@gmail.com>
2021-09-02 14:02:38 +0900

    swagger ui apply

--- app.js
+++ app.js
@@ -3,8 +3,44 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const fs = require('fs');
 var http = require('http');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+var swaggerDefinition = {
+  info: {
+    // API informations
+    title: 'NODEJS REST API SERVER SWAGGER', // Title
+    version: '0.1.0', // Version
+    description: 'node restful api', // Description
+  },
+  // host: 'localhost:3000', // Host
+  basePath: '/', // Base path
+  securityDefinitions: {
+    bearerAuth: {
+      type: '',
+      name: '',
+      in: '',
+    },
+  },
+  // security: [{ bearerAuth: [] }],
+};
+
+var customSwaggerOption = {
+  explorer: false,
+  swaggerOptions: {
+    validatorUrl: null,
+  },
+
+  // Import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // Path to the API docs
+  apis: ['./api/*.js', './api/*.yaml'],
+};
+
+// JSDoc apply
+const swaggerSpec = swaggerJSDoc(customSwaggerOption);
 
 // cors apply
 app.use(cors());
@@ -17,7 +53,8 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'content-type, Authorization');
   next();
 });
-
+// swagger api docs apply
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use(express.static(path.join(__dirname, 'build')));
 
 http.createServer(app).listen(80);
